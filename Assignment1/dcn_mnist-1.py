f857aae32035dbbe7d39377f42ef639a8170fa8d
Yikun Li <yikunli10@gmail.com>
2021-09-28 23:58:31 -0500

    update assignment1

--- Assignment1/dcn_mnist-1.py
+++ Assignment1/dcn_mnist-1.py
@@ -95,8 +95,8 @@ def main():
     # FILL IN THE CODE BELOW TO BUILD YOUR NETWORK
 
     # placeholders for input data and input labeles
-    x = tf.placeholder(tf.float32, [None, 784])
-    y_ = tf.placeholder(tf.int64, [None])
+    x = tf.placeholder(tf.float32, [None, 784], name='x')
+    y_ = tf.placeholder(tf.float32, [None, 10],  name='y_')
 
     # reshape the input image
     x_image = tf.reshape(x, [-1, 28, 28, 1])
@@ -104,57 +104,45 @@ def main():
     # first convolutional layer
     W_conv1 = weight_variable([5, 5, 1, 32])
     b_conv1 = bias_variable([32])
-    h_conv1 = tf.nn.tanh(conv2d(x_image, W_conv1) + b_conv1)
+    h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
     h_pool1 = max_pool_2x2(h_conv1)
-    variable_summaries(W_conv1)
-    variable_summaries(b_conv1)
-    variable_summaries(h_conv1)
-    variable_summaries(h_pool1)
+    # variable_summaries(W_conv1)
+    # variable_summaries(b_conv1)
+    # variable_summaries(h_conv1)
+    # variable_summaries(h_pool1)
 
     # second convolutional layer
     W_conv2 = weight_variable([5, 5, 32, 64])
     b_conv2 = bias_variable([64])
-    h_conv2 = tf.nn.tanh(conv2d(h_pool1, W_conv2) + b_conv2)
+    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
     h_pool2 = max_pool_2x2(h_conv2)
-    variable_summaries(W_conv2)
-    variable_summaries(b_conv2)
-    variable_summaries(h_conv2)
-    variable_summaries(h_pool2)
+    # variable_summaries(W_conv2)
+    # variable_summaries(b_conv2)
+    # variable_summaries(h_conv2)
+    # variable_summaries(h_pool2)
 
     # densely connected layer
     W_fc1 = weight_variable([7 * 7 * 64, 1024])
     b_fc1 = bias_variable([1024])
-    h_pool2_flat = tf.reshape(h_pool2, [-1, 7 * 7 * 64])
-    h_fc1 = tf.nn.tanh(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
-    variable_summaries(W_fc1)
-    variable_summaries(b_fc1)
-    variable_summaries(h_pool2_flat)
-    variable_summaries(h_fc1)
+    h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+    h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
     # dropout
     keep_prob = tf.placeholder(tf.float32)
     h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
-    variable_summaries(keep_prob)
-    variable_summaries(h_fc1_drop)
 
     # softmax
     W_fc2 = weight_variable([1024, 10])
     b_fc2 = bias_variable([10])
-    y_conv = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
-    variable_summaries(W_fc2)
-    variable_summaries(b_fc2)
-    variable_summaries(y_conv)
+    y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2, name='y_conv')
 
     # FILL IN THE FOLLOWING CODE TO SET UP THE TRAINING
 
     # setup training
-    cross_entropy = tf.losses.sparse_softmax_cross_entropy(
-        labels=y_, logits=y_conv)
-    cross_entropy = tf.reduce_mean(cross_entropy)
+    cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y_conv), reduction_indices=[1]))
     train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-    correct_prediction = tf.equal(tf.argmax(y_conv, 1), y_)
-    correct_prediction = tf.cast(correct_prediction, tf.float32)
-    accuracy = tf.reduce_mean(correct_prediction)
+    correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32), name='accuracy')
 
     # Add a scalar summary for the snapshot loss.
     tf.summary.scalar(cross_entropy.op.name, cross_entropy)
@@ -175,13 +163,13 @@ def main():
 
     # run the training
     for i in range(max_step):
-        batch = mnist.train.next_batch(50) # make the data batch, which is used in the training iteration.
-                                            # the batch size is 50
-        if i%100 == 0:
+        batch = mnist.train.next_batch(50)  # make the data batch, which is used in the training iteration.
+        # the batch size is 50
+        if i % 100 == 0:
             # output the training accuracy every 100 iterations
             train_accuracy = accuracy.eval(feed_dict={
-                x:batch[0], y_:batch[1], keep_prob: 1.0})
-            print("step %d, training accuracy %g"%(i, train_accuracy))
+                x: batch[0], y_: batch[1], keep_prob: 1.0})
+            print("step %d, training accuracy %g" % (i, train_accuracy))
 
             # Update the events file which is used to monitor the training (in this case,
             # only the training loss is monitored)
@@ -194,14 +182,15 @@ def main():
             checkpoint_file = os.path.join(result_dir, 'checkpoint')
             saver.save(sess, checkpoint_file, global_step=i)
 
-        train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5}) # run one train_step
+        train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})  # run one train_step
 
     # print test error
-    print("test accuracy %g"%accuracy.eval(feed_dict={
+    print("test accuracy %g" % accuracy.eval(feed_dict={
         x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
 
     stop_time = time.time()
-    print('The training takes %f second to finish'%(stop_time - start_time))
+    print('The training takes %f second to finish' % (stop_time - start_time))
+
 
 if __name__ == "__main__":
     main()
