1fb6adf62ac7e6a74a709d34084d3803a56ec126
Yikun Li <yikunli10@gmail.com>
2021-09-29 01:24:49 -0500

    update

--- Assignment1/dcn_mnist-1.py
+++ Assignment1/dcn_mnist-1.py
@@ -106,35 +106,44 @@ def main():
     b_conv1 = bias_variable([32])
     h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
     h_pool1 = max_pool_2x2(h_conv1)
-    # variable_summaries(W_conv1)
-    # variable_summaries(b_conv1)
-    # variable_summaries(h_conv1)
-    # variable_summaries(h_pool1)
+    variable_summaries(W_conv1)
+    variable_summaries(b_conv1)
+    variable_summaries(h_conv1)
+    variable_summaries(h_pool1)
 
     # second convolutional layer
     W_conv2 = weight_variable([5, 5, 32, 64])
     b_conv2 = bias_variable([64])
     h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
     h_pool2 = max_pool_2x2(h_conv2)
-    # variable_summaries(W_conv2)
-    # variable_summaries(b_conv2)
-    # variable_summaries(h_conv2)
-    # variable_summaries(h_pool2)
+    variable_summaries(W_conv2)
+    variable_summaries(b_conv2)
+    variable_summaries(h_conv2)
+    variable_summaries(h_pool2)
 
     # densely connected layer
     W_fc1 = weight_variable([7 * 7 * 64, 1024])
     b_fc1 = bias_variable([1024])
     h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
     h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+    variable_summaries(W_fc1)
+    variable_summaries(b_fc1)
+    variable_summaries(h_pool2_flat)
+    variable_summaries(h_fc1)
 
     # dropout
     keep_prob = tf.placeholder(tf.float32)
     h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+    variable_summaries(keep_prob)
+    variable_summaries(h_fc1_drop)
 
     # softmax
     W_fc2 = weight_variable([1024, 10])
     b_fc2 = bias_variable([10])
     y_conv = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2, name='y_conv')
+    variable_summaries(W_fc2)
+    variable_summaries(b_fc2)
+    variable_summaries(y_conv)
 
     # FILL IN THE FOLLOWING CODE TO SET UP THE TRAINING
 
