a772e08ecb9b63ccd85230ed5055885de450a507
Yikun Li <yikunli10@gmail.com>
2021-10-26 23:17:41 -0500

    update hw2

--- Assignment2/lstmMNISTStarterCode.py
+++ Assignment2/lstmMNISTStarterCode.py
@@ -5,46 +5,54 @@ import matplotlib.pyplot as plt
 
 from tensorflow.examples.tutorials.mnist import input_data
 
-mnist = input_data.read_data_sets('MNIST_data', one_hot=True) #call mnist function
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)  # call mnist function
 
 learningRate = 1e-3
 trainingIters = 50000
 batchSize = 100
 displayStep = 10
 
-nInput = 28 #we want the input to take the 28 pixels
-nSteps = 28 #every 28
-nHidden = 128 #number of neurons for the RNN
-nClasses = 10 #this is MNIST so you know
+nInput = 28  # we want the input to take the 28 pixels
+nSteps = 28  # every 28
+nHidden = 256  # number of neurons for the RNN
+nClasses = 10  # this is MNIST so you know
 
 x = tf.placeholder('float', [None, nSteps, nInput])
 y = tf.placeholder('float', [None, nClasses])
 
 weights = {
-	'out': tf.Variable(tf.random_normal([nHidden, nClasses]))
+    'out': tf.Variable(tf.random_normal([nHidden, nClasses]))
 }
 
 biases = {
-	'out': tf.Variable(tf.random_normal([nClasses]))
+    'out': tf.Variable(tf.random_normal([nClasses]))
 }
 
+
 def RNN(x, weights, biases):
-	x = tf.transpose(x, [1,0,2])
-	x = tf.reshape(x, [-1, nInput])
-	x = tf.split(0, nSteps, x) #configuring so you can get it as needed for the 28 pixels
+    x = tf.transpose(x, [1, 0, 2])
+    x = tf.reshape(x, [-1, nInput])
+    x = tf.split(x, nSteps, 0)  # configuring so you can get it as needed for the 28 pixels
 
-	lstmCell = rnn.BasicRNNCell(nHidden) #find which lstm to use in the documentation
+    # find which lstm to use in the documentation
+    # lstmCell = rnn_cell.BasicLSTMCell(nHidden, forget_bias=1.0)
+    # lstmCell = rnn_cell.GRUCell(nHidden)
+    lstmCell = rnn_cell.BasicRNNCell(nHidden)
 
-	outputs, states = rnn.static_rnn(lstmCell, x,
-                                     dtype=tf.float32) #for the rnn where to get the output and hidden state
+    # mixed model with RNN and LSTM
+    # lstmCell = rnn_cell.BasicRNNCell(nHidden)
 
-	return tf.matmul(outputs[-1], weights['out'])+ biases['out']
+    outputs, states = rnn.static_rnn(lstmCell, x,
+                                     dtype=tf.float32)  # for the rnn where to get the output and hidden state
+
+    return tf.matmul(outputs[-1], weights['out']) + biases['out']
+
 
 pred = RNN(x, weights, biases)
 
-#optimization
-#create the cost, optimization, evaluation, and accuracy
-#for the cost softmax_cross_entropy_with_logits seems really good
+# optimization
+# create the cost, optimization, evaluation, and accuracy
+# for the cost softmax_cross_entropy_with_logits seems really good
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))
 optimizer = tf.train.RMSPropOptimizer(learning_rate=learningRate).minimize(cost)
 
@@ -57,14 +65,14 @@ losses_list = []
 accs_list = []
 
 with tf.Session() as sess:
-	sess.run(init)
-	step = 1
+    sess.run(init)
+    step = 1
 
-	while step* batchSize < trainingIters:
-		batchX, batchY = mnist.train.next_batch(batchSize) #mnist has a way to get the next batch
-		batchX = batchX.reshape((batchSize, nSteps, nInput))
+    while step * batchSize < trainingIters:
+        batchX, batchY = mnist.train.next_batch(batchSize)  # mnist has a way to get the next batch
+        batchX = batchX.reshape((batchSize, nSteps, nInput))
 
-		sess.run(optimizer, feed_dict={x: batchX, y: batchY})
+        sess.run(optimizer, feed_dict={x: batchX, y: batchY})
 
         acc = sess.run(accuracy, feed_dict={x: batchX, y: batchY})
         loss = sess.run(cost, feed_dict={x: batchX, y: batchY})
@@ -72,16 +80,26 @@ with tf.Session() as sess:
         losses_list.append(loss)
         accs_list.append(acc)
 
-		if step % displayStep == 0:
-			print("Iter " + str(step*batchSize) + ", Minibatch Loss= " + \
-                  "{:.6f}".format() + ", Training Accuracy= " + \
-                  "{:.5f}".format())
-		step +=1
-	print('Optimization finished')
+        if step % displayStep == 0:
+            print("Iter " + str(step * batchSize) + ", Minibatch Loss= " + \
+                  "{:.6f}".format(loss) + ", Training Accuracy= " + \
+                  "{:.5f}".format(acc))
+        step += 1
+    print('Optimization finished')
 
-	testData = mnist.test.images.reshape((-1, nSteps, nInput))
-	testLabel = mnist.test.labels
-	print("Testing Accuracy:", \
-        sess.run(accuracy, feed_dict={x: testData, y: testLabel}))
+    testData = mnist.test.images.reshape((-1, nSteps, nInput))
+    testLabel = mnist.test.labels
+    print("Testing Accuracy:", sess.run(accuracy, feed_dict={x: testData, y: testLabel}))
 
     sess.close()
+
+# Plot the accuracy and loss under different parameters
+fig, ax = plt.subplots()
+ax.plot(range(len(accs_list)), accs_list, 'k', label='accuracy for 256 hidden-units')
+ax.legend(loc='upper right', shadow=True)
+plt.show()
+
+fig, bx = plt.subplots()
+bx.plot(range(len(losses_list)), losses_list, 'k', label='loss for 256 hidden-units')
+bx.legend(loc='upper right', shadow=True)
+plt.show()
--- Assignment2/trainCifarStarterCode.py
+++ Assignment2/trainCifarStarterCode.py
@@ -146,7 +146,7 @@ h_fc2 = tf.matmul(h_fc1_drop, W_fc2) + b_fc2
 # cross_entropy = tf.losses.sparse_softmax_cross_entropy(labels=tf_labels, logits=h_fc2)
 cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=tf_labels, logits=h_fc2)
 cross_entropy = tf.reduce_mean(cross_entropy)
-optimizer = tf.train.AdamOptimizer(1e-3).minimize(cross_entropy)
+optimizer = tf.train.GradientDescentOptimizer(1e-3).minimize(cross_entropy)
 # correct_prediction = tf.equal(tf.argmax(h_fc2, 1), tf_labels)
 # correct_prediction = tf.cast(correct_prediction, tf.float32)
 correct_prediction = tf.equal(tf.argmax(h_fc2, 1), tf.argmax(tf_labels, 1))
@@ -192,13 +192,15 @@ sess.close()
 
 # Plot the accuracy and loss under different parameters
 fig, ax = plt.subplots()
-ax.plot(range(len(accs_list)), accs_list, 'k', label='accuracy for AdamOptimizer')
+ax.plot(range(len(accs_list)), accs_list, 'k', label='accuracy for GradientDescentOptimizer')
 ax.legend(loc='upper right', shadow=True)
+plt.savefig('GradientDescentOptimizer accuracy')
 plt.show()
 
 fig, bx = plt.subplots()
-bx.plot(range(len(losses_list)), losses_list, 'k', label='loss for AdamOptimizer')
+bx.plot(range(len(losses_list)), losses_list, 'k', label='loss for GradientDescentOptimizer')
 bx.legend(loc='upper right', shadow=True)
+plt.savefig('GradientDescentOptimizer loss')
 plt.show()
 #
 # # Plot the filters of the first layer
